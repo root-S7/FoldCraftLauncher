6ed1353acfa84dd7c61d72ed578df43dabaf32ab
hyplant <hy18918868804@163.com>
2024-08-29 19:47:55 +0800

    fix ;

--- FCL/src/main/java/com/tungsten/fcl/ui/main/Announcement.java
+++ FCL/src/main/java/com/tungsten/fcl/ui/main/Announcement.java
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import com.tungsten.fcl.R
+import com.tungsten.fcl.R;
 import com.tungsten.fcl.upgrade.UpdateChecker;
 import com.tungsten.fcllibrary.util.LocaleUtils;
 
@@ -89,7 +89,7 @@ public class Announcement {
 
     public String getDisplayContent(Context context) {
         if (content.size() == 0) {
-            content.add(new Content("en",context.getString(R.string.announcement_failure)));
+            content.add(new Content("en",""));
         }
         for (Content c : content) {
             if (LocaleUtils.getLocale(LocaleUtils.getLanguage(context)).toString().contains(c.getLang())) {
