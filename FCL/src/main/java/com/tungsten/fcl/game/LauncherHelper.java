72517343054f4175a3b06e3d3704de966e54c7f7
人 <3020825808@qq.com>
2026-07-25 19:55:59 +0800

    删除全局设置最低内存设置选项

--- FCL/src/main/java/com/tungsten/fcl/game/LauncherHelper.java
+++ FCL/src/main/java/com/tungsten/fcl/game/LauncherHelper.java
@@ -181,7 +181,6 @@ public final class LauncherHelper {
                             Task.composeAsync(() -> null)
                     );
                 }).withStage("launch.state.dependencies")
-                .thenComposeAsync(() -> checkHardware(context)).withStage("launch.state.device")
                 .thenComposeAsync(() -> setGameRule(context, setting, rule)).withStage("launch.state.rule")
                 .thenComposeAsync(() -> {
                     try (InputStream input = LauncherHelper.class.getResourceAsStream("/assets/game/MioLibPatcher.jar")) {
@@ -275,7 +274,6 @@ public final class LauncherHelper {
                 .withStagesHint(Lang.immutableListOf(
                         "launch.state.java",
                         "launch.state.dependencies",
-                        "launch.state.device",
                         "launch.state.rule",
                         "launch.state.logging_in",
                         "launch.state.waiting_launching"))
@@ -620,27 +618,6 @@ public final class LauncherHelper {
                 });
                 return Task.fromCompletableFuture(future).thenComposeAsync(task -> task);
             }
-        });
-    }
-
-    private static Task<Boolean> checkHardware(Context context) {
-        return Task.composeAsync(() -> {
-            String memoryRequirement = FCLPath.GENERAL_SETTING.getProperty("min-memory-requirement", "4");
-            Float thisTotalMemory = (float) Math.ceil((double) MemoryUtils.getTotalDeviceMemory(context) / 1024);
-
-            int comparisonResult;
-            try {
-                comparisonResult = Float.valueOf(memoryRequirement).compareTo(thisTotalMemory);
-            } catch (Exception e) {
-                comparisonResult = Float.valueOf(4).compareTo(thisTotalMemory);
-            }
-
-            if(comparisonResult <= 0) return Task.completed(true);
-            else throw new RuntimeException(String.format(
-                    "当前设备硬件配置不符合要求\n\n运行内存至少要%sGB，而你的设备只有%dGB\n\n\n\n注：设备上的虚拟内存不能算作运行内存！",
-                    memoryRequirement,
-                    thisTotalMemory.intValue()
-            ));
         });
     }
 
